beaeb8fbdaed294c2f3cb308c05ec3afe90cecea
alievkomiljon <aliyev.komiljon@gmail.com>
2021-10-13 21:11:22 -0500

    ewsds

--- W3/D3_Maze/maze.js
+++ W3/D3_Maze/maze.js
@@ -1,18 +1,21 @@
 $(function(){
+    let isStarted = false;
     let isTouchedTheWalls = false;
     let isFinished = false;
 
     $('.boundary').mouseover(function(){
-        if(!isTouchedTheWalls && !isFinished){
+        if(!isTouchedTheWalls && !isFinished && isStarted){
             $('.boundary').addClass('youlose');
             isTouchedTheWalls = true;
+            isStarted = false;
             alertLost();
         }
     });
     $('#maze').mouseleave(function(){
-        if(!isTouchedTheWalls && !isFinished){
+        if(!isTouchedTheWalls && !isFinished && isStarted){
             $('.boundary').addClass('youlose');
             isTouchedTheWalls = true;
+            isStarted = false;
             alertLost();
         }
     });
@@ -23,14 +26,17 @@ $(function(){
     }
 
     $('#end').mouseover(function(){
-        if(!isTouchedTheWalls && !isFinished){
+        if(!isTouchedTheWalls && !isFinished && isStarted){
             alert('You won!\nPress S to start again');
             isFinished = true;
+            isStarted = false;
         }
     });
 
     $('#start').click(function(){
         isTouchedTheWalls = false;
+        isStarted = true;
+        isFinished = false;
         $('.boundary').removeClass('youlose');
     });
 
